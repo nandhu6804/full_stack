20a696e9a240921a82d959bb69bda0454319cf46
Nandhitha <nandhithap.22cse@kongu.edu>
2024-12-29 22:40:27 +0530

    day3 commit2

--- fullstack-proj/src/App.jsx
+++ fullstack-proj/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ClassCompEg from './Components/ClassComponents/classCompEg'
+import FunctionalCompEg from './Components/FunctionalComponents/FunctionalCompEg'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-
+      <ClassCompEg />
+      <FunctionalCompEg />
     </div>
   );
 }
--- fullstack-proj/src/Components/ClassComponents/classCompEg.jsx
+++ fullstack-proj/src/Components/ClassComponents/classCompEg.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from 'react';
 
-class classCompEg extends React.Component {
-  return (
-    <div></div>
-  );
+class ClassCompEg extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>This is a class component</h1>
+        <ul>
+          <li>React</li>
+          <li>JavaScript</li>
+          <li>HTML</li>
+          <li>CSS</li>
+        </ul>
+      </div>
+    );
+  }
 }
+
+export default ClassCompEg;
