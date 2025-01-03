aa8fbf731005aa98eb69e294fc50a011346c0938
Nandhitha <nandhithap.22cse@kongu.edu>
2025-01-03 10:02:48 +0530

    commit1

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,29 @@
-var express =require('express')
+var express = require('express')
 var path = require('path')
-app=express()
-const PORT =3001;
-app.get('/',(req,res)=>{
+var app = express()
+var mdb = require('mongoose')
+const PORT = 3001;
+
+mdb.connect("mongodb://localhost:27017").then(() => {
+    console.log("MongoDB Connection Successful...");
+}).catch(() => {
+    console.log("Check Your COnnection String...");
+})
+
+app.get('/', (req, res) => {
     res.json('hello!!!let\'s learn')
 })
-app.get('/json',(req,res)=>{
-    res.json({server:'hello!!!',url:'localhost',port:'3001'})
+app.get('/json', (req, res) => {
+    res.json({ server: 'hello!!!', url: 'localhost', port: '3001' })
 })
 app.get('/static', (req, res) => {
-    res.sendFile(path.join(__dirname,'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/html2', (req, res) => {
-    res.sendFile(path.join(__dirname,'index2.html'));
+    res.sendFile(path.join(__dirname, 'index2.html'));
 });
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log('Backend Server Started\nURL: http://localhost:3001')
 })
