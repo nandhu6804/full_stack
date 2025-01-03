e32eb81cfb70aa5497f83c479c302ce8cc2b3362
Nandhitha <nandhithap.22cse@kongu.edu>
2025-01-03 09:39:31 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,21 @@
-var express = require('express')
-var app = express()
-const PORT = 3001
-
-/*app.get('/',(req,res)=>{
-    res.send("Welcome here....")//prints first
-})*/
-
-app.get('/', (req, res) => {
-    res.send("Welcome....")//notprints
+var express =require('express')
+var path = require('path')
+app=express()
+const PORT =3001;
+app.get('/',(req,res)=>{
+    res.json('hello!!!let\'s learn')
 })
-
-app.get('/json', (req, res) => {
-    res.json({ server: "Welcome....", url: "localhost", port: "3001" })//localhost:3001/json
+app.get('/json',(req,res)=>{
+    res.json({server:'hello!!!',url:'localhost',port:'3001'})
 })
-
 app.get('/static', (req, res) => {
-    res.sendFile('C:\Users\Nandhitha\Videos\fullstack\backend\index.html')
-})
+    res.sendFile(path.join(__dirname,'index.html'));
+});
 
+app.get('/html2', (req, res) => {
+    res.sendFile(path.join(__dirname,'index2.html'));
+});
 
-app.listen(PORT, () => {
-    console.log(`Backend server started...\nURL: http://localhost:${PORT}`)
+app.listen(PORT,()=>{
+    console.log('Backend Server Started\nURL: http://localhost:3001')
 })
